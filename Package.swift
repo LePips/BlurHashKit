c0bd7423398de68cbeb3f99bff70f79c38bf36ab
Ethan Pippin <ethanpippin2343@gmail.com>
2022-09-27 21:19:28 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 let package = Package(
     name: "BlurHashKit",
     platforms: [
-        .macOS(.v10_13),
         .iOS(.v13),
         .tvOS(.v13),
         .watchOS(.v4),
